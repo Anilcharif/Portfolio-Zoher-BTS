8a7162b787cd10870bbcdbc5dca09a17f019bf9c
ANIL Charif Zoher <anilcharif06@gmail.com>
2022-11-14 00:07:13 +0100

    ,

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("loaded");
-    let btn = document.querySelector(".burger");
-    let el = document.querySelector("header div");
-    let close1 = document.querySelector(".close");
 
-    let plus = document.querySelector(".plus1");
-    let el1 = document.querySelector("main div.close-menu");
 
-    btn.addEventListener("click", () => {
-        el.classList.toggle("open-menu");
-        btn.innerHTML === "☰" ? btn.innerHTML = "✕" : btn.innerHTML = "☰";
-    });
-    close1.addEventListener("click", () => {
-        el.classList.toggle("open-menu");
-        btn.innerHTML === "☰" ? btn.innerHTML = "✕" : btn.innerHTML = "☰";
-    });
+    
+    const menuShowBtn = document.getElementById('afficher_btn');
+    const menuHideBtn = document.getElementById('cacher_btn');
+    const menuOverlay = document.querySelector('.nav_overlay');
+    const svg = document.querySelector('svg');
 
-    plus.addEventListener("click", () => {
-        el1.classList.toggle("open-plus");
-        plus.innerHTML === "+ Plus" ? plus.innerHTML = "- Moins" : plus.innerHTML = "+ Plus";
-        
-    });
-});
+    menuShowBtn.addEventListener('click', () => { 
+        menuOverlay.classList.add('afficher_overlay')    
+        svg.style.display = 'none'; 
+    })
+    menuHideBtn.addEventListener("click", () =>{
+        menuOverlay.classList.remove('afficher_overlay')
+        svg.style.display = 'block';
+
+    })
+
+
+    
+
+  });
